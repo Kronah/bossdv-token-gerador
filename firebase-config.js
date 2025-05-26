82b48058d665894e4f24fbff8c1d811abfcce0bc
Kronah <kronah.contas@gmail.com>
2025-05-25 21:23:43 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // firebase-config.js
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
+  apiKey: "AIzaSyDVwW_stGdkYpArCLjqsDbBsUiaSihGsUE",
   authDomain: "bossdv-72d16.firebaseapp.com",
   databaseURL: "https://bossdv-72d16-default-rtdb.firebaseio.com ",
   projectId: "bossdv-72d16",
   storageBucket: "bossdv-72d16.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  messagingSenderId: "141528827265",
+  appId: "1:141528827265:android:2c1074201e8b0550869480"
 };
 
 // Inicializa Firebase
